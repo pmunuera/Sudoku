033201f309ca9b73e355ff894554721406475d3e
pmunuera <95918506+pmunuera@users.noreply.github.com>
2022-10-21 18:52:18 +0200

    Commit sudoku

--- app/src/main/java/com/example/sudoku/MainActivity.java
+++ app/src/main/java/com/example/sudoku/MainActivity.java
@@ -17,21 +17,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TableLayout table = (TableLayout) findViewById(R.id.textView);
-        CharSequence[] nombres = {"0","1","2","3","4","5","6","7","8","9"};
-        TableRow row=new TableRow(this);
+        CharSequence[] nombres = {"|   |","1","2","3","4","5","6","7","8","9"};
+        table.setDividerPadding(10);
         for(int i=0;i<9;i++) {
-            TableRow.LayoutParams  params1=new TableRow.LayoutParams();
+            TableRow.LayoutParams  params1=new TableRow.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT,10.0f);
+            TableRow row=new TableRow(this);
+
             for(int j=0;j<9;j++) {
                 Spinner spinner = new Spinner(this);
-                TextView txt1 = new TextView(this);
-                txt1.setText(nombres[j]);
-                row.addView(txt1);
-                row.setLayoutParams(params1);
+                spinner.setBackground(null);
+                spinner.setPadding(5, 5, 5, 5);
 
                 ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this,
                         android.R.layout.simple_spinner_item, nombres);
                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 spinner.setAdapter(adapter);
+                spinner.setTag(R.id.col,i);
+                spinner.setTag(R.id.fila,j);
+                row.addView(spinner);
+                row.setLayoutParams(params1);
             }
             table.addView(row);
         }
