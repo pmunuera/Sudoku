428573117bff53b945b7bf147af6925200f753f1
pmunuera <95918506+pmunuera@users.noreply.github.com>
2022-10-21 19:09:52 +0200

    Commit sudoku

--- app/src/main/java/com/example/sudoku/MainActivity.java
+++ app/src/main/java/com/example/sudoku/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.sudoku;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TableLayout;
@@ -16,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        int[][] values = new int[9][9];
         TableLayout table = (TableLayout) findViewById(R.id.textView);
         CharSequence[] nombres = {"|   |","1","2","3","4","5","6","7","8","9"};
         table.setDividerPadding(10);
@@ -24,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
             TableRow row=new TableRow(this);
 
             for(int j=0;j<9;j++) {
+                values[i][j]=j;
                 Spinner spinner = new Spinner(this);
                 spinner.setBackground(null);
                 spinner.setPadding(5, 5, 5, 5);
@@ -32,12 +37,34 @@ public class MainActivity extends AppCompatActivity {
                         android.R.layout.simple_spinner_item, nombres);
                 adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 spinner.setAdapter(adapter);
-                spinner.setTag(R.id.col,i);
-                spinner.setTag(R.id.fila,j);
+                spinner.setTag(R.id.col,j);
+                spinner.setTag(R.id.fila,i);
                 row.addView(spinner);
                 row.setLayoutParams(params1);
             }
             table.addView(row);
+        }
+
+
+    }
+    public class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener {
+
+        protected void onCreate(Bundle savedInstanceState) {
+
+            super.onCreate(savedInstanceState);
+            Spinner spinner = new Spinner(this);
+            spinner.setOnItemSelectedListener(this);
+
+        }
+
+        public void onItemSelected(AdapterView<?> parent, View view,
+                                   int pos, long id) {
+            // An item was selected. You can retrieve the selected item using
+            // parent.getItemAtPosition(pos)
+        }
+
+        public void onNothingSelected(AdapterView<?> parent) {
+            // Another interface callback
         }
     }
 }
